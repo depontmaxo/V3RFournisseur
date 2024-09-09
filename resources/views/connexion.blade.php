eb6c10e5dcc139c49eb1e872f8a3d4f16020af26
QUEBEC987 <isaac_bd@outlook.com>
2024-09-09 08:33:57 -0400

    petite modif

--- resources/views/connexion.blade.php
+++ resources/views/connexion.blade.php
@@ -15,12 +15,6 @@
                 <a>Créer un compte</a>  
             </nav>    
         </header>
-        
-        <div>
-            <h1>Connexion</h1>
-        </div>
-        
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <div class="container-fluid text-center">
             <h1 class="titre2">Connexion</h1>
         </div>
@@ -43,6 +37,7 @@
                 </div> 
             </div> 
         </form>
-
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
