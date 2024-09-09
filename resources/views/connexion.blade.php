6950ef0df3c36b0e904d8ac6c58138721649d57b
depontmax <maxojojo2@gmail.com>
2024-09-09 08:27:53 -0400

    création du controller D'utilisateurs

--- resources/views/connexion.blade.php
+++ resources/views/connexion.blade.php
@@ -6,8 +6,40 @@
         <title>Connexion</title>
     </head>
     <body>
+        <header>
+            <div>
+                <h5 class="compagny">Fournisseur</h5>
+            </div>
+            <nav class="sub-nav">                
+                <a>Créer un compte</a>  
+            </nav>    
+        </header>
+        
         <div>
             <h1>Connexion</h1>
         </div>
+        <div class="container-fluid text-center">
+            <h1 class="titre2">Connexion</h1>
+        </div>
+        <form method="post" action="{{ route('Tutorat.index_login') }}">
+            @csrf
+            <div class="d-flex row justify-content-center py-5">
+                <div class="form-group">
+                <label for="email" class="titre2">Email</label>
+                <input type="email" class="form-control" id="email" placeholder="email" name="email">
+                </div> 
+                <div class="d-flex row justify-content-center">
+                <div class="form-group">
+                    <label for="password" class="titre2">Password</label>
+                    <input type="password" class="form-control" id="password" placeholder="password" name="password">
+                </div> 
+                </div> 
+                <div class="d-flex row justify-content-center">
+                <div class="form-group">
+                    <button type="submit" class="btn btn-secondary">Connexion</button>
+                </div> 
+            </div> 
+        </form>
+
     </body>
 </html>
