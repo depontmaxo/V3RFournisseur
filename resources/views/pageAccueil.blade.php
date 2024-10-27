ff8f2f316b26e694c9c07fa1225accafbda35512
QUEBEC987 <isaac_bd@outlook.com>
2024-10-27 13:20:42 -0400

    Ajout logo page accueil

--- resources/views/pageAccueil.blade.php
+++ resources/views/pageAccueil.blade.php
@@ -22,11 +22,8 @@
             </div>
         </div>
         
-        <div class="menu">
-            <div>
-                <img>
-            </div>
-
+        <div>
+            <img class="logo" src="../images/logoVilleTR.png">
         </div>
         
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
