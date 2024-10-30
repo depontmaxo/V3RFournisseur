533b4bcbf058c2dad934d0069ca2462b36275f08
QUEBEC987 <isaac_bd@outlook.com>
2024-10-30 16:19:15 -0400

    Corrections petits bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,56 +48,14 @@
 
     @else
         <p>Accès non authorisé</p>
-        <a href="{{route('Connexion.connexionNEQ')}}">Retourner à la page de connexion</a>
+        <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            // Redirige vers la route Laravel
+            window.location.href = "{{ route('Connexion.connexion') }}";
+        });
+    </script>
     @endif
-    <!--<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        @if (Auth::user()->role == 'responsable') 
-            <a class="navbar-brand" href="{{ route('Responsable.index') }}">Navbar</a>
-        @elseif (Auth::user()->role == 'admin')
-            <a class="navbar-brand" href="{{ route('Responsable.index') }}">Navbar</a>
-        @else
-            <a class="navbar-brand" href="{{ route('Fournisseur.index') }}">Navbar</a>
-        @endif
-
-        
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-
-            @if (Auth::user()->role == 'responsable') 
-                <a class="nav-link active" aria-current="page" href="{{ route('Responsable.index') }}">Home</a>
-            @elseif (Auth::user()->role == 'admin')
-                <a class="nav-link active" aria-current="page" href="{{ route('Responsable.index') }}">Home</a>
-            @else
-                <a class="nav-link active" aria-current="page" href="{{ route('Fournisseur.index') }}">Home</a>
-            @endif
-
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-        </ul>
-        </div>
-    </div>
-    </nav>-->
+    
 
     <footer>
         </br>
